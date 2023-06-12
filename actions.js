bb535d2f9f52b56569b88292fe70142206f63c81
Jordan <JDLChang@gmail.com>
2023-06-12 14:10:34 -0500

    adds js functionality and starts Desktop mode

--- actions.js
+++ actions.js
@@ -1,26 +1,41 @@
-const signUpEmail = getElementByClass(".signUpEmail");
-const filledEmail = getElementById(".filledEmail");
-const dismiss = querySelector("dismissBtn");
+const emailInput = document.querySelector("input[type='email']");
+const submitBtn = document.querySelector(".subBtn");
+const success = document.querySelector(".successPop");
+
+const errorSign = document.querySelector(".errorSign");
+
+const dismissBtn = document.getElementById("dismissBtn");
+const filledEmail = document.querySelector(".filledEmail");
+
+function isValidEmail(email) {
+  // Regular expression to validate email format
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+}
+
+//adds error class if email is invalid OR displays success
+ submitBtn.addEventListener("click", () => {
+  const email = emailInput.value;
+  if (isValidEmail(email)) {
+    success.style.display = "flex";
+    filledEmail.innerHTML = email;
+  } else {
+    emailInput.classList.add("error");
+    errorSign.style.display ="block";
+    console.log("Please provide a valid email address");
+  }
+ });
+
+ //removes .error class when user starts typing
+  emailInput.addEventListener("input", () => {
+  if (emailInput.classList.contains("error")) {
+    emailInput.classList.remove("error");
+    errorSign.style.display ="none";
+  }
+});
 
 
-// function returnEmail(){
-//     addEventListener("input", () => {
-//         signUpEmail.style.display = "none";
-//         filledEmail.style.display = "block";
-//     } )
-// }
 
-function appear(){
-    console.log('appear works')
-    signUpEmail.addEventListener((onclick),() => {
-        filledEmail.style.display = "block";
-    }   
-)}
-
-
-function disappear(){
-    console.log('disppear works')
-    dismiss.addEventListener((onclick),() => {
-        filledEmail.style.display = "none";
-    }   
-)};
+dismissBtn.addEventListener("click", () => {
+  success.style.display = "none";
+});
