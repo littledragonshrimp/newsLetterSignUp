fcf6d75fbe29c5fda14d5977710d4645cdbb4e32
Jordan <JDLChang@gmail.com>
2023-06-13 14:40:42 -0500

    adds js functionality, desktop mode, positions successPop

--- actions.js
+++ actions.js
@@ -2,6 +2,12 @@ const emailInput = document.querySelector("input[type='email']");
 const submitBtn = document.querySelector(".subBtn");
 const success = document.querySelector(".successPop");
 
+const full = document.querySelector(".fullHero");
+const content = document.querySelector(".content");
+const main = document.querySelector("main");
+
+
+
 const errorSign = document.querySelector(".errorSign");
 
 const dismissBtn = document.getElementById("dismissBtn");
@@ -19,6 +25,10 @@ function isValidEmail(email) {
   if (isValidEmail(email)) {
     success.style.display = "flex";
     filledEmail.innerHTML = email;
+    content.classList.add("gone");
+    main.classList.add("disappearBackground");
+    full.classList.add("gone") //makes main content disappear on desktop
+    
   } else {
     emailInput.classList.add("error");
     errorSign.style.display ="block";
@@ -35,7 +45,12 @@ function isValidEmail(email) {
 });
 
 
-
+//removes .gone
 dismissBtn.addEventListener("click", () => {
   success.style.display = "none";
+  if (content.classList.contains("gone")) {
+    content.classList.remove("gone");
+    full.classList.remove("gone");
+    main.classList.remove("disappearBackground");
+  }
 });
